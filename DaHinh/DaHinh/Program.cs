d8577d763eb15aaed51c61c3182fd3076cf12fa8
VanTay2k1 <nguyenvantay0601@gmail.com>
2022-04-19 08:44:29 +0700

    bai da hinh

---
+++ DaHinh/DaHinh/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaHinh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ DaHinh/DaHinh/hinh.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaHinh
+{
+    class hinh
+    {
+        private int socanh;
+        private int sodinh;
+
+        public void socanh()
+        {
+
+        }
+    }
+}
