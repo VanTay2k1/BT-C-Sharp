2add986fc390acdf360dc03c98f06e6f6bc8c22e
VanTay2k1 <nguyenvantay0601@gmail.com>
2022-05-10 07:41:14 +0700

    diem danh ngay 10/5

---
+++ BtDiemDanh/Bai1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap a,b,c: ");
+            double a150 = Int32.Parse(Console.ReadLine());
+            double b150 = Int32.Parse(Console.ReadLine());
+            double c150 = Int32.Parse(Console.ReadLine());
+            ptb2 pt = new ptb2(a150, b150, c150);
+
+            double delta = pt.delta(a150, b150, c150);
+            int check150 = pt.nghiem(delta);
+            if (check150 < 0)
+            {
+                Console.WriteLine("Pt vo nghiem");
+            }
+            else if (check150 == 0)
+            {
+                double x = -b150 / (2 * a150);
+                Console.WriteLine("pt co nghiem kep: ", x);
+            }
+            else
+            {
+                double x1 = (-b150 - Math.Sqrt(delta)) / (2 * a150);
+                double x2 = (-b150 + Math.Sqrt(delta)) / (2 * a150);
+
+                Console.WriteLine("Pt co 2 nghiem x1={0} , x2={1}: ", x1, x2);
+            }
+        }
+    }
+
+    class ptb2
+    {
+        private double a150;
+        private double b150;
+        private double c150;
+
+        public ptb2()
+        {
+        }
+
+        public ptb2(double a150, double b150, double c150)
+        {
+            A = a150;
+            B = b150;
+            C = c150;
+        }
+
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        public double delta(double a150, double b150, double c150)
+        {
+            return b150 * b150 - 4 * a150 * c150;
+        }
+
+        public int nghiem(double delta150)
+        {
+            if (delta150 < 0)
+            {
+                return -1;
+            }
+            else if (delta150 == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+}
---
+++ DiemDanh_10-5/DiemDanh_10-5/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiemDanh_10-5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr_150 = new int[100];
+            int i_150, mx_150, mn_150;
+
+
+            Console.Write("Nhap {0} phan tu vao trong mang:\n", 10);
+            for (i_150 = 0; i_150 < 10; i_150++)
+            {
+                Console.Write("Phan tu - {0}: ", i_150);
+                arr_150[i_150] = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+            mx_150 = arr_150[0];
+            mn_150 = arr_150[0];
+
+            for (i_150 = 1; i_150 < 10; i_150++)
+            {
+                if (arr_150[i_150] > mx_150)
+                {
+                    mx_150 = arr_150[i_150];
+                }
+
+
+                if (arr_150[i_150] < mn_150)
+                {
+                    mn_150 = arr_150[i_150];
+                }
+            }
+            Console.Write("Phan tu lon nhat la: {0}\n", mx_150);
+            Console.Write("Phan tu nho nhat la: {0}\n\n", mn_150);
+
+            for (i_150 = 0; i_150 < 10; i_150++)
+            {
+                for (int j = i_150 + 1; j < 10; j++)
+                {
+                    if (arr_150[i_150] > arr_150[j])
+                    {
+                        // Nếu arr[i] > arr[j] thì hoán đổi giá trị của arr[i] và arr[j]
+                        int temp_122 = arr_150[i_150];
+                        arr_150[i_150] = arr_150[j];
+                        arr_150[j] = temp_122;
+                    }
+                }
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(arr_150[i] + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
